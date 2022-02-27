9990ae1e7569405c47a207c7dd00e6d013bb13be
Bandit <rex978956@gmail.com>
2022-02-28 04:00:49 +0800

    [fix] auto

--- Python/setup.sh
+++ Python/setup.sh
@@ -2,7 +2,7 @@
 #   Version 
 #   Author: Bandit
 #   GitHub: github.com/rex978956
-#   python3.10
+#   install python3.10.2 to replace python3.8.10
 
 CurrentUser=$1
 HomeDirectory=$2
@@ -27,6 +27,9 @@ apt install python3.10 python3.10-dev python3.10-venv -y
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 2
 
+apt remove python3-apt -y
+apt autoclean -y
+apt install python3-apt -y
 
 # function run_scripts()
 # {
