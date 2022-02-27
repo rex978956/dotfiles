29432a8dc053c45ce86dee3c3a68c0555e8b5610
Bandit <rex978956@gmail.com>
2022-02-28 03:11:06 +0800

    [feat] add batcat install script

---
+++ Misc/batcat.sh
@@ -0,0 +1,9 @@
+wget https://github.com/sharkdp/bat/releases/download/v0.20.0/bat_0.20.0_amd64.deb -O /tmp/bat_install.deb
+
+dpkg -i /tmp/bat_install.deb
+
+rm bat_install.deb
+
+if ( command -v bat > /dev/null 2>&1 ); then
+    echo "=> $(bat -V) installed."
+fi
