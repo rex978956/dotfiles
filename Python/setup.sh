d7d77e3e1ef4698f839998e45215e374cdbe3f37
Bandit <rex978956@gmail.com>
2022-02-28 03:48:36 +0800

    [fix] auto

--- Python/setup.sh
+++ Python/setup.sh
@@ -22,9 +22,7 @@ function initial()
 apt install software-properties-common -y
 add-apt-repository ppa:deadsnakes/ppa -y
 apt update
-apt install python3.10
-apt install python3.10-dev
-apt install python3.10-venv
+apt install python3.10 python3.10-dev python3.10-venv -y
 
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 2
