d2a6b93c9c4975954ee15b7cf25a84f6a3a1fcd3
Bandit <rex978956@gmail.com>
2022-02-28 04:09:41 +0800

    [fix] rm file

--- Misc/batcat.sh
+++ Misc/batcat.sh
@@ -2,7 +2,7 @@ wget https://github.com/sharkdp/bat/releases/download/v0.20.0/bat_0.20.0_amd64.d
 
 dpkg -i /tmp/bat_install.deb
 
-rm bat_install.deb
+rm /tmp/bat_install.deb
 
 if ( command -v bat > /dev/null 2>&1 ); then
     echo "=> $(bat -V) installed."
