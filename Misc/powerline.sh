e43139bf9996aa4909bc9f4588afd4227df440f1
Bandit <rex978956@gmail.com>
2022-02-28 22:40:13 +0800

    [fix] powerline path

--- Misc/powerline.sh
+++ Misc/powerline.sh
@@ -2,7 +2,7 @@
 # https://gist.github.com/leosuncin/25bad6ae66c5d513b986
 
 git clone https://github.com/powerline/fonts.git --depth=1 /tmp/fonts
-./tmp/fonts/install.sh
+/tmp/fonts/install.sh
 rm -rf /tmp/fonts
 
 echo -e "[\e[0;32mINFO\e[0m] powerline installed."
