c53bb447fc0a3ff38dddea6f4f9161049117de1c
Bandit <rex978956@gmail.com>
2022-02-28 15:45:00 +0800

    [feat] add color centent

--- setup.sh
+++ setup.sh
@@ -4,16 +4,40 @@
 #   GitHub: github.com/rex978956
 #
 
-# Absolute path to this script, z.B. /home/bandit/dotfiles/install.sh
-SCRIPT=$(readlink -f "$0")
-# Absolute path this script is in, thus /home/bandit/dotfiles
-ScriptLocation=$(dirname "$SCRIPT")
+# colors
+RED='\e[0;31m'
+GREEN='\e[0;32m'
+YELLOW='\e[1;33m'
+NC='\e[0m'
+function echo_failure()
+{
+    echo -e "${RED}[Failure] $1${NC}"
+}
 
+function echo_success()
+{
+    echo -e "${GREEN}[Success] $1${NC}"
+}
+
+function echo_warning()
+{
+    echo -e "${YELLOW}[Warning] $1${NC}"
+}
+
+function echo_info()
+{
+    echo -e "[${GREEN}INFO${NC}] $1"
+}
+
+
+# Defines
+SCRIPT=$(readlink -f "$0")
+ScriptLocation=$(dirname "$SCRIPT")
 CurrentUser=$USER
 HomeDirectory=$HOME
 
+# Checking distribution
 source ./Script/check_distribution.sh
-echo "your distribution is $distribution $distribution_version"
 
 softwares=(git python-is-python3 python3-pip zsh vim tmux curl exuberant-ctags ack-grep autojump gcc gdb fonts-powerline nautilus)
 
@@ -25,45 +49,31 @@ function initial()
         permission=$USER
         CurrentUser=$SUDO_USER
         HomeDirectory="/home/$SUDO_USER"
-    elif [ "$distribution" == "CentOS Linux" ]; then # Not guaranteed to perform
-        permission=$USER
-        CurrentUser=$SUDO_USER
-        HomeDirectory="/home/$SUDO_USER"
-    elif [ "$distribution" == "Kali GNU/Linux" ]; then # Not guaranteed to perform
-        permission=$USER
-        CurrentUser=$SUDO_USER
-        if [ "$SUDO_USER" == "" ]; then
-            CurrentUser=$permission
-        fi
-        HomeDirectory="/home/bandit"
     else
-        echo "Your distribution havn't been support yet. exit.."
+        echo_failure "Only supports Ubuntu. exit.."
         exit 1
     fi
 
-    echo "Enter Current Username: ($CurrentUser)"
-    read
+    read -e -p "${YELLOW}Enter Current Username: ${NC}" -i "$CurrentUser"
     if [ "$REPLY" != "" ]; then
         CurrentUser=$REPLY
     fi
 
-    echo "Enter Home Directory: ($HomeDirectory)"
-    read
+    read -e -p "${YELLOW}Enter Home Directory: ${NC}" -i "$HomeDirectory"
     if [ "$REPLY" != "" ]; then
         HomeDirectory=$REPLY
     fi
 
-    echo "Enter Script Location: ($ScriptLocation)"
-    read
+    read -e -p "${YELLOW}Enter Script Location: ${NC}" -i "$ScriptLocation"
     if [ "$REPLY" != "" ]; then
         ScriptLocation=$REPLY
     fi
 
-    echo "========================================================"
-    echo "Home Directory: $HomeDirectory"
-    echo "Current Username: $CurrentUser"
-    echo "Script Location: $ScriptLocation"
-    read -p "Are you sure? (y/N)" -n 1 -r
+    echo "========================================================"\
+    echo_info "Current Username: $CurrentUser"
+    echo_info "Home Directory: $HomeDirectory"
+    echo_info "Script Location: $ScriptLocation"
+    read -p "${YELLOW}Are you sure? (y/N): ${NC}"
     if [[ ! $REPLY =~ ^[Yy]$ ]]
     then
         exit 1
@@ -73,45 +83,42 @@ function initial()
 function install_dotfiles_folder()
 {
     if [ -e "$HomeDirectory/dotfiles" ]; then
-        echo "dotfiles in $HomeDirectory existed"
+        echo_info "dotfiles in $HomeDirectory existed"
     else
-        echo "Link $ScriptLocation to $HomeDirectory"
+        echo_info "Link $ScriptLocation to $HomeDirectory"
         ln -s $ScriptLocation $HomeDirectory
     fi
 }
 
 function check_software()
 {
-    echo "checking $1..."
+    echo_info "checking $1..."
     if [ -x "`which $1`" ]; then
-        echo "Done."
+        echo_info "checking: $1 already exists."
     else
-        echo "$1 is not installed. installing..."
-        if [ "$distribution" == "Ubuntu" ]; then
-            apt install -y $1
-        elif [ "$distribution" == "CentOS Linux" ]; then
-            yum -y install $1
-        fi
+        echo_info "checking: $1 is not installed. installing..."
+        apt install -y $1
     fi
 }
 
 # scipt start
 
-echo ""
-echo "  +------------------------------------------------+"
-echo "  |                                                |\\"
-echo "  |       Bandit's dotfile install script       | \\"
-echo "  |                                                | |"
-echo "  +------------------------------------------------+ |"
-echo "   \\______________________________________________\\|"
-echo ""
-echo "copy from WildfootW, thank you WildfootW!"
+echo_info " "
+echo_info "   +------------------------------------------------+"
+echo_info "   |                                                |\\"
+echo_info "   |       Bandit's dotfile install script       | \\"
+echo_info "   |                                                | |"
+echo_info "   +------------------------------------------------+ |"
+echo_info "    \\______________________________________________\\|"
+echo_info " "
+echo_info "Your distribution is ${distribution:?} ${distribution_version:?}"
+echo " "
 
 initial
 
 # check sudo
 if [ $permission != "root" ]; then
-    echo -e "\e[1;4;5m[!] Warning: You need to be sudo..., exit.\e[0m"
+    echo_failure "You need to be sudo..., exit."
     exit 1
 fi
 
@@ -126,7 +133,7 @@ locale-gen zh_TW.UTF-8
 update-locale
 
 # clone submodule
-echo "Cloning submodule..."
+echo_info "Cloning submodule..."
 git submodule init
 git submodule update
 
@@ -137,7 +144,8 @@ for subfolder in ${subfolders[@]}; do
     bash $ScriptLocation/$subfolder/setup.sh $CurrentUser $HomeDirectory $ScriptLocation
 done
 
-echo "Done! but maybe you need to restart your computer."
+echo_success "Setup Done!"
+echo_warning "But maybe you need to restart your computer."
 
 # make workplace dir
 # echo "create workplace directory"
