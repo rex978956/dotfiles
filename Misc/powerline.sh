0047b974a46a73521aee2a68d45df593b5ebdbf8
Bandit <rex978956@gmail.com>
2022-02-28 21:36:18 +0800

    [fix] execute error

--- Misc/powerline.sh
+++ Misc/powerline.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 # https://gist.github.com/leosuncin/25bad6ae66c5d513b986
 
-
 git clone https://github.com/powerline/fonts.git --depth=1 /tmp/fonts
-bash /tmp/fonts/install.sh
+./tmp/fonts/install.sh
 rm -rf /tmp/fonts
 
 echo -e "[\e[0;32mINFO\e[0m] powerline installed."
