4e7160205acb6c51df5ea0f955237aaa496aad41
Bandit <rex978956@gmail.com>
2022-02-28 20:01:05 +0800

    [fix] homedirectoy error

--- Python/setup.sh
+++ Python/setup.sh
@@ -25,15 +25,14 @@ apt-get update -y
 apt-get install python3 python3-pip python3-dev -y 
 apt-get install -y --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+git clone https://github.com/pyenv/pyenv.git $HomeDirectory/.pyenv
+omz reload
 pyenv rehash
 git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
 pyenv install 3.10.2
 pyenv global 3.10.2
 
 python3 -m pip install --upgrade pip
-
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 
 pkgs=(pwntools pycryptodome requests) 
 
